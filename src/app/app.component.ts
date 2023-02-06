323903810948410a4c14045e07e79951310ccc99
onowakow <nowakowskiowen08@gmail.com>
2023-02-06 09:14:50 -0700

    Move directions$ to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { DirectionsService } from './directions.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private _directionsService: DirectionsService) {}
+
+  directions$ = this._directionsService.directions$;
+}
